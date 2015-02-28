7b107bca7476da76529b82b5586428d7c84f4a7c
Alessandro Siletto <alessandro@siletto.it>
2015-02-28 18:45:06 +0100

    sync local repositories to amazon s3

---
+++ push.sh
@@ -0,0 +1,25 @@
+#
+#cd /d/dev/workspaces/personale/economy2/market-doc
+#jgit.sh push s3 master
+#
+#
+#cd /d/dev/workspaces/personale/economy2/market-model
+#jgit.sh push s3 master
+#
+#
+cd /d/dev/workspaces/personale/economy2/registry-model
+jgit.sh push s3 master
+#
+#
+#
+cd /d/dev/workspaces/personale/economy2/currency-model
+jgit.sh push s3 master
+#
+cd /d/dev/workspaces/personale/economy2/currency-service
+jgit.sh push s3 master
+#
+#
+cd /d/dev/workspaces/personale/economy2/currency-client
+jgit.sh push s3 master
+#
+read -p "Press any key to close..."
